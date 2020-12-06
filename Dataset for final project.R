16daa9edad4712f30e73c9d72dbbdf8c3d8a5d65
Nathan Wondim <nathanw2020@Nathans-MacBook-Pro.local>
2020-12-06 12:23:02 -0800

    Files created for Final

--- Dataset for final project.R
+++ Dataset for final project.R
@@ -1,3 +1,5 @@
 df <- read.csv("https://query.data.world/s/mqdkdczdtdpefieaq4qur5i6l74d7i")
 
 #if you want to download the dataset here's the link: https://query.data.world/s/45ui36bkdmfxcwzbvhmbd7k3qpttjn
+
+# Link to summary of dataset: https://data.world/ahalps/which-social-media-millennials-care-about-most/workspace/data-dictionary
