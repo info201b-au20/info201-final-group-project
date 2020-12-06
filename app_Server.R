8bd87e007d1885868feb5fe4efff78d7948ab0b7
sggebre1-2028601 <72582487+sggebre1-2028601@users.noreply.github.com>
2020-12-06 12:53:18 -0800

    Update app_Server.R

--- app_Server.R
+++ app_Server.R
@@ -1,5 +1,10 @@
 
 
 server <- function(input, output, session) {
-  
+  output$chart <- renderPlotly({ 
+        
+       chart <- ggplot(data = newDF) +
+        geom_point(mapping = aes_string(x = input$x_input, y = "Preferred_type_of_communication"),
+                   color = input$color_input) 
+        ggplotly(chart)
 }
