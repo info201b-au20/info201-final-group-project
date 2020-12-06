92f907b3d3ff3072058829671979526c87368a31
Nathan Wondim <nathanw2020@Nathans-MacBook-Pro.local>
2020-12-06 05:08:09 -0800

    New Dataset to use for Final Project

---
+++ Dataset for final project.R
@@ -0,0 +1,3 @@
+df <- read.csv("https://query.data.world/s/mqdkdczdtdpefieaq4qur5i6l74d7i")
+
+#if you want to download the dataset here's the link: https://query.data.world/s/45ui36bkdmfxcwzbvhmbd7k3qpttjn
