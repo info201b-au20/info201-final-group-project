296402aad9a22ea8c704d73ccefbcaa88276ec03
Sam Akeyo <akeyosa@uw.edu>
2020-12-13 13:24:45 -0800

    update

--- app_Server.R
+++ app_Server.R
@@ -1,10 +1,28 @@
 library(plotly)
 
 server <- function(input, output, session) {
-  output$chart <- renderPlotly({ 
-        chart <- ggplot(data = newDF) +
-        geom_point(mapping = aes_string(x = input$x_input, y = "Preferred_type_of_communication"),
-                   color = input$color_input) 
-        ggplotly(chart)
-  })
+	output$socialmedia_vs_exercise <- renderPlotly({ 
+		ggplot(new_dataframe, aes(x = id)) + 
+		geom_line(aes(y = Time_spent_on_social_media_in_a_day, color = "darkred"), size = 3) + 
+		geom_point(aes(y = Time_spent_on_physical_activities_in_a_day, color="steelblue"), size = 2) +
+		scale_color_manual(values = c("darkred", "steelblue"), labels = c("Hours on social media", "Hours on physical activity")) +
+		labs(title = "Social media use vs physical activity", x = "Survey Entry ID (ordered from least to greatest by hours on social media)", y = "Hours")
+
+		socialmedia_vs_exercise <- plotly_build(socialmedia_vs_exercise)
+		socialmedia_vs_exercise$x$data[[1]]$name <- "Time on social media"
+		socialmedia_vs_exercise$x$data[[2]]$name <- "Time on physical activity"
+	})
+	output$effects <- renderPlotly({ 
+		ggplot(new_dataframe, aes(x = Time_spent_on_social_media_in_a_day)) +
+			geom_bar() +
+			labs(title = "Time Spent on Social Media Per Day", x = "range of hours", y = "frequency")
+  
+		effects <- ggplotly(effects)
+	})
+	output$chart <- renderPlotly({ 
+		chart <- ggplot(data = newDF) +
+		geom_point(mapping = aes_string(x = input$x_input, y = "Preferred_type_of_communication"),
+				   color = input$color_input) 
+		ggplotly(chart)
+	})
 }  
--- app_UI.R
+++ app_UI.R
@@ -6,6 +6,68 @@ library(shinyWidgets)
 
 originalDF <- read.csv("https://raw.githubusercontent.com/info201b-au20/info201-final-group-project/gh-pages/Effects%20of%20Social%20Media%20(Responses).csv")
 
+######################################Chart1######################################################################
+new_dataframe <- originalDF %>% #changed "dataframe" to "originalDF"
+  rename(Age = "What.is.your.age.",
+         Social_media_accounts = "Which.social.media.platform.s.do.you.like.the.most.or.use.the.most.",
+         Time_spent_on_physical_activities_in_a_day = "How.much.time.do.you.spend.on.physical.activities.in.a.day.",,
+         Time_spent_on_social_media_in_a_day = "How.much.time.do.you.spend.on.social.media.in.a.day.") %>% 
+  select(Age,Social_media_accounts,Time_spent_on_physical_activities_in_a_day,Time_spent_on_social_media_in_a_day) %>% 
+  group_by(Age)
+
+#Changes made to Time_spent_on_phsical activities_in_a_day column to later convert the characters to integers
+new_dataframe$Time_spent_on_physical_activities_in_a_day <- str_replace(new_dataframe$Time_spent_on_physical_activities_in_a_day, "none","0")
+new_dataframe$Time_spent_on_physical_activities_in_a_day <- str_replace(new_dataframe$Time_spent_on_physical_activities_in_a_day, "less than 1 hr","0")
+new_dataframe$Time_spent_on_physical_activities_in_a_day <- str_replace(new_dataframe$Time_spent_on_physical_activities_in_a_day, "upto 4 hrs","4")
+new_dataframe$Time_spent_on_physical_activities_in_a_day <- str_replace(new_dataframe$Time_spent_on_physical_activities_in_a_day, "1 - 2.5 hrs","2")
+new_dataframe$Time_spent_on_physical_activities_in_a_day <- str_replace(new_dataframe$Time_spent_on_physical_activities_in_a_day, "more than 4 hrs","4")
+
+#Converts the strings to integers in the physical activities column 
+new_dataframe$Time_spent_on_physical_activities_in_a_day <- as.integer(new_dataframe$Time_spent_on_physical_activities_in_a_day)            
+
+#Changes made to Tine_spent_on_social_media_in_a_day column to later convert the characters to integers
+new_dataframe$Time_spent_on_social_media_in_a_day <- str_replace(new_dataframe$Time_spent_on_social_media_in_a_day, "none","0")
+new_dataframe$Time_spent_on_social_media_in_a_day <- str_replace(new_dataframe$Time_spent_on_social_media_in_a_day, "less than 1 hr","0")
+new_dataframe$Time_spent_on_social_media_in_a_day <- str_replace(new_dataframe$Time_spent_on_social_media_in_a_day, "upto 4 hrs","4")
+new_dataframe$Time_spent_on_social_media_in_a_day <- str_replace(new_dataframe$Time_spent_on_social_media_in_a_day, "1 - 2.5 hrs","2")
+new_dataframe$Time_spent_on_social_media_in_a_day <- str_replace(new_dataframe$Time_spent_on_social_media_in_a_day, "more than 4 hrs","4")
+
+#Converts the strings to integers in the time_spent_on_social_media column
+new_dataframe$Time_spent_on_social_media_in_a_day <- as.integer(new_dataframe$Time_spent_on_social_media_in_a_day)            
+
+#Sort dataframe by hours on social media
+new_dataframe <- arrange(new_dataframe, Time_spent_on_social_media_in_a_day)
+
+#Add column for row number
+new_dataframe$id <- seq.int(nrow(new_dataframe))
+
+socialmedia_vs_exercise <- ggplot(new_dataframe, aes(x = id)) + 
+  geom_line(aes(y = Time_spent_on_social_media_in_a_day, color = "darkred"), size = 3) + 
+  geom_point(aes(y = Time_spent_on_physical_activities_in_a_day, color="steelblue"), size = 2) +
+  scale_color_manual(values = c("darkred", "steelblue"), labels = c("Hours on social media", "Hours on physical activity")) +
+  labs(title = "Social media use vs physical activity", x = "Survey Entry ID (ordered from least to greatest by hours on social media)", y = "Hours")
+
+socialmedia_vs_exercise <- plotly_build(socialmedia_vs_exercise)
+socialmedia_vs_exercise$x$data[[1]]$name <- "Time on social media"
+socialmedia_vs_exercise$x$data[[2]]$name <- "Time on physical activity"
+
+####################################Chart2#######################################################################
+new_dataframe <- originalDF %>% #changed "dataframe" to "originalDF"
+  rename(Age = "What.is.your.age.",
+         Social_media_accounts = "Which.social.media.platform.s.do.you.like.the.most.or.use.the.most.",
+         Time_spent_on_social_media_in_a_day = "How.much.time.do.you.spend.on.social.media.in.a.day.",
+         Preferred_type_of_communication = "Which.type.of.communication.do.you.generally.prefer.") %>% 
+  select(Age,Social_media_accounts,Time_spent_on_social_media_in_a_day,Preferred_type_of_communication) %>% 
+  group_by(Age)
+
+effects <- ggplot(new_dataframe, aes(x = Time_spent_on_social_media_in_a_day)) +
+    geom_bar() +
+  labs(title = "Time Spent on Social Media Per Day", x = "range of hours", y = "frequency")
+  
+effects <- ggplotly(effects)
+
+####################################Chart3########################################################################
+
 newDF <- originalDF %>% 
     rename(Age = "What.is.your.age.",
            Social_media_accounts = "Which.social.media.platform.s.do.you.like.the.most.or.use.the.most.",
@@ -57,27 +119,30 @@ Intro_page <- tabPanel(
 )
   
 Interactive_Page_1 <- tabPanel(
-  titlePanel("Interactive Page 1"),
+  titlePanel("Social media use vs Physical activity per day"),
   sidebarLayout(
     sidebarPanel(
-        
-    ),
+            p("This chart shows, on average how much time college aged students spend on social media and physical
+			activity per day. The chart shows that almost no students spend zero time on social media. The chart also 
+			shows that about two thirds of college aged students (undergrad) spend what we'd call a reasonable amount
+			of time on social media per day. Conversley, the other third spend an excessive amount of time on social media.")),
     mainPanel(
-        
+            plotlyOutput("socialmedia_vs_exercise")
     )
   )
 )
 
+
 Interactive_Page_2 <- tabPanel(
-  titlePanel("Interactive Page 2"),
+  titlePanel("Time on social media per day"),
   sidebarLayout(
     sidebarPanel(
-      
-    ),
+            p("test")),
     mainPanel(
-      
+            (plotlyOutput("effects")
     )
   )
+)
 )
 
 Interactive_Page_3 <- tabPanel(
