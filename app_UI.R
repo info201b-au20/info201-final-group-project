ec4a5a8fc7bbfc9586c30bf58fa4129e6db926da
Sam Akeyo <akeyosa@uw.edu>
2020-12-13 13:39:25 -0800

    update

--- app_UI.R
+++ app_UI.R
@@ -41,15 +41,6 @@ new_dataframe <- arrange(new_dataframe, Time_spent_on_social_media_in_a_day)
 #Add column for row number
 new_dataframe$id <- seq.int(nrow(new_dataframe))
 
-socialmedia_vs_exercise <- ggplot(new_dataframe, aes(x = id)) + 
-  geom_line(aes(y = Time_spent_on_social_media_in_a_day, color = "darkred"), size = 3) + 
-  geom_point(aes(y = Time_spent_on_physical_activities_in_a_day, color="steelblue"), size = 2) +
-  scale_color_manual(values = c("darkred", "steelblue"), labels = c("Hours on social media", "Hours on physical activity")) +
-  labs(title = "Social media use vs physical activity", x = "Survey Entry ID (ordered from least to greatest by hours on social media)", y = "Hours")
-
-socialmedia_vs_exercise <- plotly_build(socialmedia_vs_exercise)
-socialmedia_vs_exercise$x$data[[1]]$name <- "Time on social media"
-socialmedia_vs_exercise$x$data[[2]]$name <- "Time on physical activity"
 
 ####################################Chart2#######################################################################
 new_dataframe <- originalDF %>% #changed "dataframe" to "originalDF"
@@ -59,12 +50,6 @@ new_dataframe <- originalDF %>% #changed "dataframe" to "originalDF"
          Preferred_type_of_communication = "Which.type.of.communication.do.you.generally.prefer.") %>% 
   select(Age,Social_media_accounts,Time_spent_on_social_media_in_a_day,Preferred_type_of_communication) %>% 
   group_by(Age)
-
-effects <- ggplot(new_dataframe, aes(x = Time_spent_on_social_media_in_a_day)) +
-    geom_bar() +
-  labs(title = "Time Spent on Social Media Per Day", x = "range of hours", y = "frequency")
-  
-effects <- ggplotly(effects)
 
 ####################################Chart3########################################################################
 
