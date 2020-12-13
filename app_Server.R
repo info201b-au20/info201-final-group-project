a04543310edff4108d03c16736ff4ec3e075ab2b
Sam Akeyo <akeyosa@uw.edu>
2020-12-13 13:58:40 -0800

    update

--- app_Server.R
+++ app_Server.R
@@ -8,9 +8,8 @@ server <- function(input, output, session) {
 		scale_color_manual(values = c("darkred", "steelblue"), labels = c("Hours on social media", "Hours on physical activity")) +
 		labs(title = "Social media use vs physical activity", x = "Survey Entry ID (ordered from least to greatest by hours on social media)", y = "Hours")
 
-		socialmedia_vs_exercise <- plotly_build(socialmedia_vs_exercise)
-		socialmedia_vs_exercise$x$data[[1]]$name <- "Time on social media"
-		socialmedia_vs_exercise$x$data[[2]]$name <- "Time on physical activity"
+		socialmedia_vs_exercise <- ggplotly(socialmedia_vs_exercise)
+		
 	})
 	output$effects <- renderPlotly({ 
 		ggplot(new_dataframe, aes(x = Time_spent_on_social_media_in_a_day)) +
