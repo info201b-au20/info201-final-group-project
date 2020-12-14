11aa8e7960843d967f3b5109dd55322cc630aeb3
Nathan Wondim <nathanw2020@Nathans-MacBook-Pro.local>
2020-12-13 17:15:52 -0800

    Update

--- app_Server.R
+++ app_Server.R
@@ -41,10 +41,12 @@ new_df_chart1$id <- seq.int(nrow(new_df_chart1))
 		socialmedia_vs_exercise <- ggplot(new_df_chart1, aes(x = id)) + 
 		geom_line(aes(y = Time_spent_on_social_media_in_a_day), color = "darkred", size = input$size) + 
 		geom_point(aes(y = Time_spent_on_physical_activities_in_a_day), color = "steelblue", size = input$size2) +
-		#scale_color_manual(values = c("darkred", "steelblue"), labels = c("Hours on social media", "Hours on physical activity")) +
+		scale_color_manual(values = c("darkred", "steelblue"), labels = c("Hours on social media", "Hours on physical activity")) +
 		labs(title = "Social media use vs physical activity", x = "Survey Entry ID (ordered from least to greatest by hours on social media)", y = "Hours")
     
-	  
+		socialmedia_vs_exercise <- plotly_build(socialmedia_vs_exercise)
+		socialmedia_vs_exercise$x$data[[1]]$name <- "Time on social media"
+		socialmedia_vs_exercise$x$data[[2]]$name <- "Time on physical activity"
 	  
 		socialmedia_vs_exercise <- ggplotly(socialmedia_vs_exercise)
 		
--- app_UI.R
+++ app_UI.R
@@ -40,16 +40,6 @@ new_dataframe <- arrange(new_dataframe, Time_spent_on_social_media_in_a_day)
 
 #Add column for row number
 new_dataframe$id <- seq.int(nrow(new_dataframe))
-
-socialmedia_vs_exercise <- ggplot(new_dataframe, aes(x = id)) + 
-  geom_line(aes(y = Time_spent_on_social_media_in_a_day), size = 3) + 
-  geom_point(aes(y = Time_spent_on_physical_activities_in_a_day), size = 2) +
-  #scale_color_manual(values = c("darkred", "steelblue"), labels = c("Hours on social media", "Hours on physical activity")) +
-  labs(title = "Social media use vs physical activity", x = "Survey Entry ID (ordered from least to greatest by hours on social media)", y = "Hours")
-
-  socialmedia_vs_exercise <- plotly_build(socialmedia_vs_exercise)
-  socialmedia_vs_exercise$x$data[[1]]$name <- "Time on social media"
-  socialmedia_vs_exercise$x$data[[2]]$name <- "Time on physical activity"
   
 
 ####################################Chart2#######################################################################
@@ -213,7 +203,7 @@ Conclusion_Page <- tabPanel(
   people are using are Youtube, Twitter, Instagram, and Snapchat."),
 
     shiny::tags$h3("What age group spends the most time on social media throughout the day? "),
-  p("According to the servey, the age group that spends the most time on social
+  p("According to the survey, the age group that spends the most time on social
   media throughout the day is 19 year olds. "),
 
     shiny::tags$h3("3 Major Takeaways"),
